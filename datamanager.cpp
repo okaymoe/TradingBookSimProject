ef8518cad783947f6318030b8bd48b059e9ab4a6
Moe <moe62197@gmail.com>
2025-03-18 02:08:00 -0400

    datamanager header

---
+++ datamanager.cpp
@@ -0,0 +1,29 @@
+#ifndef DATAMANAGER_H
+#define DATAMANAGER_H
+
+#include <QObject>
+#include <QNetworkAccessManager>
+#include <QTimer>
+
+class DataManager : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(double currentPrice READ currentPrice NOTIFY currentPriceChanged)
+public:
+    explicit DataManager(QObject *parent = nullptr);
+    double currentPrice() const { return m_currentPrice; }
+
+signals:
+    void currentPriceChanged();
+
+private slots:
+    void fetchPrice();
+    void handleNetworkReply();
+
+private:
+    double m_currentPrice = 0.0;
+    QNetworkAccessManager m_manager;
+    QTimer m_timer;
+};
+
+#endif
